987ebd1330f1ed7986011772a92e7bcc9772b267
Rharison Lucas <rharison.abreu@gmail.com>
2022-07-01 13:33:50 -0300

    Filtering 2015 life expectancy by country

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log('oi')
+const fs = require('fs')
+
+fs.readFile('./csv/life_expectance.csv', 'utf8', (err, data) => {
+  const originalDataArray = data.split(/\r?\n/);
+  const filteredData = originalDataArray.filter(data => data.split(',')[1] === '2015')
+  const organizedDataByCountry = filteredData.map(data => {
+    return {
+      country: data.split(',')[0],
+      lifeExpectancy: Math.round(+(data.split(',')[2]))
+    }
+  })
+  fs.writeFileSync('./json/life-expectance-by-country.json', JSON.stringify(organizedDataByCountry, null, 2));
+
+});
